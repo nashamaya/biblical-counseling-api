4dc56285b750fab37422f5b8ea9c01e96f094555
nashamaya <nashamaya@yahoo.com>
2024-12-13 22:14:07 -0800

    Added Topics API

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const Emotion = require('./contents/emotion');
+const Topic = require('./contents/topics');
 
 const app = express()
 
@@ -32,6 +33,21 @@ app.get('/api/verses', async (req, res) => {
     }   
 })
 
+// API endpoint to get verses by topic
+app.get('/api/topics', async (req, res) => {
+    const topic = req.query.topic;
+    if (!topic) return res.status(400).json({ error: 'Topic is required' });
+
+try {
+    const result = await Topic.findOne({ topic });
+    if (!result) return res.status(404).json({ error: 'Topic not found' });
+    res.json(result)
+} catch (err) {
+    res.status(500).json({ error: 'Server error' });
+}
+})
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+
 
